c8d77d22b9bbf577ade5d998edecd895d4d5a082
Stina <christinama92@gmail.com>
2021-02-17 16:58:32 +0000

    styling

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,21 @@ import './App.css';
 function App() {
 	const [ kanyeQuote, setKanyeQuote ] = useState('');
 
-	useEffect(() => {
-		async function getQuote() {
+	useEffect(()=>{
+		getQuote();
+	}, []);
+
+	async function getQuote() {
 			const res = await fetch(process.env.REACT_APP_KANYE_API_URL);
 			const data = await res.json();
+			console.log(data);
 
 			setKanyeQuote(data.quote);
 		}
+
+	function handleClick() {
 		getQuote();
-	}, []);
+	};
 
 	return (
 		<div className='App'>
@@ -20,9 +26,10 @@ function App() {
 				<h1>Living Your Kanye Best</h1>
 				<img
 					src='https://static.billboard.com/files/media/kanye-west-whh-2018-billboard-1548-compressed.jpg'
-					alt='Kanye'
+					alt='Kanye' className = "App-logo"
 				/>
 				<p>{kanyeQuote}</p>
+				<button className= 'button' onClick = {handleClick}>Zen your way to Kanye's Wisdom</button>
 			</header>
 		</div>
 	);
