7b70ac241e9133e5a4f25ab5da200a62d8a98723
Stina <christinama92@gmail.com>
2021-02-17 15:25:39 +0000

    enviro variable

--- kanye-app/src/components/App.js
+++ kanye-app/src/components/App.js
@@ -6,7 +6,7 @@ function App() {
 
 	useEffect(() => {
 		async function getQuote() {
-			const res = await fetch("https://api.kanye.rest");
+			const res = await fetch(process.env.REACT_APP_KANYE_API_URL);
 			const data = await res.json();
      
 			setKanyeQuote(data.quote);
@@ -17,6 +17,8 @@ function App() {
 	return (
 		<div className='App'>
 			<header className='App-header'>
+				<h1>Living Your Kanye Best</h1>
+				<img src = 'https://static.billboard.com/files/media/kanye-west-whh-2018-billboard-1548-compressed.jpg' alt = 'Kanye'></img>
 				<p>{kanyeQuote}</p>
 			</header>
 		</div>
